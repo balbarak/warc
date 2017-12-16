18525fe89e12459f38b6f6f297f7adaa75a11ea6
balbarak <balbarak@live.com>
2017-12-17 02:33:37 +0300

    uo

---
+++ src/Warc/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Warc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var warcFilePath = @"C:\Repos\warc\data\arabicweb16.categorized.content.warc";
+
+            List<WarcItem> items = new List<WarcItem>();
+
+            Console.WriteLine("Reading ...");
+
+            var text = File.ReadAllText(warcFilePath);
+
+            var warcItems = ExtractWarCItems(text);
+
+            int count = warcItems.Count;
+            int index = 1;
+
+            foreach (var item in warcItems)
+            {
+                Console.WriteLine($"Proccessing {index} of {count}");
+
+                items.Add(WarcItem.Create(item));
+
+                index++;
+            }
+
+            Save(items);
+
+        }
+
+
+        public static List<string> ExtractWarCItems(string data)
+        {
+            var result = new List<string>();
+            
+            var pattern = "WARC/1.0(.|\n)*?</html>";
+
+            var matches = Regex.Matches(data, pattern);
+
+            foreach (Match item in matches)
+            {
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+
+        public static void Save(List<WarcItem> items)
+        {
+            int index = 1;
+            int count = items.Count;
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"Proccessing {index} of {count}");
+
+                item.Save();
+
+                index++;
+            }
+        }
+    }
+}
---
+++ src/Warc/WarcItem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Warc
+{
+    public class WarcItem
+    {
+        public string DocId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string Url { get; set; }
+
+        public string Header { get; set; }
+
+        public string Body { get; set; }
+
+        public string BodyStrip { get; set; }
+
+        public static WarcItem Create(string data)
+        {
+            var body = ExtractBody(data);
+            return new WarcItem()
+            {
+                DocId = ExtractDocId(data),
+                Body  = body,
+                BodyStrip = StripHtml(body)
+            };
+        }
+
+        private static string StripHtml(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+                return null;
+
+            string result = Regex.Replace(text, "<(.|\\n)*?>", string.Empty);
+            result = result.Replace("&nbsp;", " ");
+            return result;
+        }
+
+        private static string ExtractDocId(string data)
+        {
+            string result = "";
+
+            var pattern = "WARC-DOC-ID:(.+)";
+
+            var row = Regex.Match(data, pattern)?.Value;
+
+            var split = row.Split(':');
+
+            if (split.Length > 1)
+                result = split[1];
+
+            return result;
+        }
+
+        public static string ExtractBody(string data)
+        {
+            var result = "";
+
+            var bodyPattern = "<boSuccessfuly>(.|\n)*?</boSuccessfuly>";
+
+            var match = Regex.Match(data, bodyPattern)?.Value;
+
+            if (!String.IsNullOrWhiteSpace(match))
+                result = match;
+
+            return result;
+        }
+
+        public void Save()
+        {
+            if (String.IsNullOrWhiteSpace(Body))
+                return;
+
+            var output = $"output\\{DocId}.txt";
+
+            File.WriteAllText(output,BodyStrip);
+        }
+    }
+}
